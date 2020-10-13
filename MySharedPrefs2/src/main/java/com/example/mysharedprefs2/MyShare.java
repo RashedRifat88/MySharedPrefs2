9d2aa7653bd28e732d97f901ce10ff02e386cabe
Ras Rif <rasrif88@gmail.com>
2020-10-13 16:06:39 +0600

    Initial commit

---
+++ MySharedPrefs2/src/main/java/com/example/mysharedprefs2/MyShare.java
@@ -0,0 +1,12 @@
+package com.example.mysharedprefs2;
+
+import android.content.Context;
+
+public class MyShare {
+
+    static Context context;
+
+    public static void initialise(Context mContext){
+        context = mContext;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MySharedPrefs2
+ 
